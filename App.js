896fd012e44d18a18b90557afbf6bc0af3b2798f
papa-react <makiya112545@gmail.com>
2022-02-28 18:45:08 +0300

    map data from local and render

--- App.js
+++ App.js
@@ -1,34 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Button, Text, SafeAreaView } from "react-native";
-
-const styles = StyleSheet.create({
-	container: {
-		width: "100%",
-		height: "100%",
-		backgroundColor: "#e5e5e5",
-	},
-	info: {
-		marginTop: "20%",
-		width: "100%",
-		alignItems: "center",
-	},
-	title: {
-		fontSize: "35px",
-		fontWeight: "600",
-	},
-	price: {
-		fontSize: 16,
-		color: "#5c5e62",
-	},
-});
-
+import { SafeAreaView } from "react-native";
+import CarItem from "./components/carItem";
+import styles from "./components/carItem/style";
+import cars from "./components/carItem/cars";
 export default function App() {
 	return (
 		<SafeAreaView style={styles.container}>
-			<View style={styles.info}>
-				<Text style={styles.title}>Model S</Text>
-				<Text style={styles.price}>Starting at $99,000 </Text>
-			</View>
+			{cars.map((car) => (
+				<CarItem
+					key={car.name}
+					image={car.image}
+					name={car.name}
+					tagline={car.tagline}
+					taglineCTA={car.taglineCTA}
+				/>
+			))}
 		</SafeAreaView>
 	);
 }
